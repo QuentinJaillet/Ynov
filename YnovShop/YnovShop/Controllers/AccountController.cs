85b31aff90a98c138806dd1248009ac59b7515f8
Quentin Jaillet <jaillet.quentin@outlook.fr>
2018-01-18 23:00:12 +0100

    Add accountController

---
+++ YnovShop/YnovShop/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YnovShop.Models;
+
+namespace YnovShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ YnovShop/YnovShop/Models/AccountModels.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YnovShop.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Veuillez indiquer votre prénom")]
+        [Display(Name = "Prénom")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Veuillez indiquer votre nom")]
+        [Display(Name = "Nom")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Veuillez indiquer votre email")]
+        [EmailAddress]
+        [Display(Name = "Email")]   
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)] 
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe")]  // todo resource 
+        [Compare("Password", ErrorMessage = "Le mot de passe ne correspond pas")]
+        public string ConfirmPassword { get; set; }
+    }
+}
