26cfab4a88f6b9de7849829531cb0a1a9e0aa09f
Quentin Jaillet <jaillet.quentin@outlook.fr>
2018-01-18 22:27:29 +0100

    Db mapping

---
+++ YnovShop/YnovShop.Data/Entities/YUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YnovShop.Data.Entities
+{
+    public partial class YUser
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public DateTime Registration { get; set; }
+        public bool IsEnable { get; set; }
+    }
+}
---
+++ YnovShop/YnovShop.Data/Entities/YnovShopContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace YnovShop.Data.Entities
+{
+    public partial class YnovShopContext : DbContext
+    {
+        public virtual DbSet<YUser> YUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=YnovShop;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<YUser>(entity =>
+            {
+                entity.ToTable("y_user");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Firstname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lastname)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salt)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
